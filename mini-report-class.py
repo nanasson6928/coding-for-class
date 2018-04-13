743d4217cd861cea42cd6fe9349438b2d2983e09
nanasson6928 <wjddk6928@outlook.kr>
2018-04-13 18:29:22 +0900

    Add mini-report-class.py

---
+++ mini-report-class.py
@@ -0,0 +1,28 @@
+
+name1 = "성안고"
+foundation = 1999
+address = "경기도 안산시 상록구 용신로 170"
+
+name2 = "상록고"
+foundation2 = 2013
+address2 = "경기도 안산시 상록구 반석로4길 34"
+
+name3 = "동산고"
+foundation3 = 1994
+address3 = "경기도 안산시 상록구 충장로 56"
+
+
+class School:
+    name = "성안고"
+    foundation = 1999
+    address = "경기도 안산시 상록구 용신로 170"
+
+# School을 새로 생성할 때 마다 name,foundation,address를 입력하도록 함
+    def __init__(self, name, foundation, address):
+        self.name = name
+        self.foundation = foundation
+        self.address = address
+
+school1 = School("성안고",1999,"경기도 안산시 상록구 용신로 170")
+school2 = School("상록고",2013,"경기도 안산시 상록구 반석로4길 34")
+school3 = School("동산고",1994, "경기도 안산시 상록구 충장로 56")
