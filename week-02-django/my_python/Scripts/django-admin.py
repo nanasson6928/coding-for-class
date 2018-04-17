7f0ff3043798ccb72568902820d4e80ba76293a3
nanasson6928 <wjddk6928@outlook.kr>
2018-04-17 19:14:30 +0900

    Add week-02-django project
    - started blog app

---
+++ week-02-django/my_python/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\wjddk\dev\coding-for-office-workers\week-02-django\my_python\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
