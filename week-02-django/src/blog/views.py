ebdbcfc026ab3bd17be318a6fee0959d522c7679
nanasson6928 <wjddk6928@outlook.kr>
2018-04-23 22:12:28 +0900

    updated 20180423

--- week-02-django/src/blog/views.py
+++ week-02-django/src/blog/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 # from random import randint
+from .models import Article
 # Create your views here.
 
 
@@ -9,5 +10,15 @@ def index(request):
     # random_number = randint(1, 10)
     # return HttpResponse("Hello, world. {}".format(random_number))
     # return HttpResponse("Hello, world. You're at the blog index")
-    name = "wjddk"
-    return render(request, "index.html", { "name" : name})
+    # name = "wjddk"
+    # return render(request, "index.html", { "name" : name})
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title = "hello",
+    #     contents = "this is test",
+    #     view_count = 0
+    # )
+    ctx = {
+        "article_list" : article_list
+    }
+    return render(request, "index.html", ctx)
